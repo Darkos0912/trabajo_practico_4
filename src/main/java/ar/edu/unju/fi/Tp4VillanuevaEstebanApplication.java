1e71657b17e79645a14e9f9e60d255d19ac9fa84
Darkos0912 <villanuevadarkos@gmail.com>
2024-05-27 13:18:22 -0300

    Base del Tp4 - Esteban Villanueva

---
+++ src/main/java/ar/edu/unju/fi/Tp4VillanuevaEstebanApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4VillanuevaEstebanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4VillanuevaEstebanApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,99 @@
+package ar.edu.unju.fi.model;
+
+import java.util.Calendar;
+
+public class Alumno {
+	private int dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private int telefono;
+	private Calendar fechaNacimiento;
+	private String domicilio;
+	private String lu;
+	
+	public Alumno(){
+		// TODO Auto-generated constructor stub
+	}
+
+	public Alumno(int dni, String nombre, String apellido, String email, int telefono, Calendar fechaNacimiento,
+			String domicilio, String lu) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.fechaNacimiento = fechaNacimiento;
+		this.domicilio = domicilio;
+		this.lu = lu;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public Calendar getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Calendar fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public String getLu() {
+		return lu;
+	}
+
+	public void setLu(String lu) {
+		this.lu = lu;
+	}
+	
+	
+	
+	
+}
+
